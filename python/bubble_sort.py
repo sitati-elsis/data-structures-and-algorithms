d52714ae404d93224dfe70c890eef1ff9a3cf3b9
sitati-elsis <sitatielsis@gmail.com>
2018-09-24 11:18:24 +0300

    refactor bubble sort

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,21 +1,17 @@
 
 
 def bubble_sort(array):
-    result = array
     list_size = len(array)
-    # loop trough all elements of the array
-    for i in list(range(list_size)):
-        for j in list(range(list_size-i-1)):
-            # go through the array until the last sorted element
-            # swap if the element found is greater than the next element
-            if array[j] > array[j+1]:
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
-                # array[j], array[j+1] = array[j+1], array[j]
-    return result
+    # loop through the list n times
+    for count in list(range(list_size)):
+        # go throught the list until the sorted part
+        # swap if the element found is greater than the next element
+        for item in list(range(list_size - count - 1)):
+            if array[item] > array[item + 1]:
+                array[item], array[item +1 ] = array[item + 1], array[item]
+        
+    return array
 
-    
 # Driver code to test above
 arr = [64, 34, 25, 12, 22, 11, 90]
 print(bubble_sort(arr))
