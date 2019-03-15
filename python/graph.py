b216a5382a9e7ab7e6e7d7e2a3bcc83a9b34b970
sitati-elsis <sitatielsis@gmail.com>
2019-03-15 12:37:01 +0300

    make new chages

--- python/graph.py
+++ python/graph.py
@@ -1,57 +1,39 @@
-# main function and creation of graph
+# implementation of an undirected graph using Adjacency Lists
 
-def generate_edges(graph):
-    edges = []
-    for nodes in graph:
-        for neighbour in graph[nodes]:
-            edges.append((nodes, neighbour))
-    return edges
+class Vertex:
 
+    def __init__(self, value):
+        self.value = value
+        self.neighbours = []
 
-def find_path_two_vertices(graph,start, end, path=[]):
-    # adding first node to path
-    path = path + [start]
-    if start == end:
-        return path
-    if not start in graph:
-        return None
-    # if start and end are different
-    for node in graph[start]:
-        if node not in path:
-            newpath = find_path_two_vertices(graph, node, end, path)
-            if newpath:
-                return newpath
-    return None
+    def add_neighbour(self, vertex):
+        if vertex not in self.neighbours:
+            self.neighbours.append(vertex)
+            self.neighbours.sort()
 
 
-def shortest_path_two_vertices(graph, start, end, path=[]):
-    path = path + [start]
-    if start == end:
-        return path
-    if not start in graph:
-        return start
-    shortest_path = None
-    for node in graph[start]:
-        if node not in path:
-            newpath = shortest_path_two_vertices(graph, node, end, path)
-            if newpath:
-                if not shortest_path or len(shortest_path) < len(newpath):
-                    shortest = newpath
+class Graph:
 
-    return shortest
-if __name__ == "__main__":
-    graph = {
-        'A': ['A', 'B', 'C'],
-        'B': ['C', 'D'],
-        'C': ['D'],
-        'D': ['C'],
-        'E': ['F'],
-        'G': [],
-        'F': []
-    }
+    def __init__(self):
+        self.vertices = {}
 
-    print(graph['A'])
-    print ("The nodes/vertices of the graph", graph.keys, "\n The edges of graph: for keys{0}, are {1}".format(graph.keys(), graph.values()))
-    print("The generated edges are :", generate_edges(graph))
-    print("Path between two vertices:", find_path_two_vertices(graph, 'A', 'D', path=[]))
-    print("Shortest path between two vertices", shortest_path_two_vertices(graph, 'A', 'D', path=[]))
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in vertices:
+            self.vertices[vertex.name] = vertex
+            return True
+        else:
+            return False
+
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.vertices and vertex2 in self.vertices:
+            for key, value in self.vertices.items():
+                if key = vertex1:
+                    value.add_neighbour(vertex2)
+                if key = vertex2:
+                    value.add_neighbour(vertex1)
+            return True
+        else:
+            return False
+
+
+
